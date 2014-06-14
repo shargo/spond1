406b28d8aa9660516c5849833e9333b7acba96e1
Atal Shargorodsky <shargorodsky@gmail.com>
2014-06-14 15:30:32 +0300

    rubbish

--- packages/buildroot/tools/customize-rootfs-real.sh
+++ packages/buildroot/tools/customize-rootfs-real.sh
@@ -87,8 +87,8 @@ copy_all_spond_files() {
 	rm -rf usr/local/bin/cgminer
 	cp ${CUR_DIR}/../../cgminer/src/cgminer etc/bin/
 	cp ${CUR_DIR}/../../spilib/src/miner_gate_test_arm usr/local/bin
-	cp ${CUR_DIR}/../../spilib/zabbix_reader/zabbix_reader_arm  usr/local/bin
-	cp ${CUR_DIR}/../../spilib/hammer_reg/reg usr/local/bin
+	cp ${CUR_DIR}/../../spilib/src/zabbix_reader/zabbix_reader_arm  usr/local/bin
+	cp ${CUR_DIR}/../../spilib/src/hammer_reg/reg usr/local/bin
 	#cp ${CUR_DIR}/../add-ons/mining_controller usr/local/bin
 	date > build_date.txt
 	echo 2 > etc/mg_work_mode
