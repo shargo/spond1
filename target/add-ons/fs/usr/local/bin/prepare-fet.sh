14c4cd28c27ce05f751146d106699d714abd7336
Remo <rimonl@gmail.com>
2014-08-29 01:37:58 +0300

    adapt prepare-fet.sh for 3 phases dcdc

--- target/add-ons/fs/usr/local/bin/prepare-fet.sh
+++ target/add-ons/fs/usr/local/bin/prepare-fet.sh
@@ -2,28 +2,62 @@
 
 #currently it's pretty stupid.
 #when board vpd is supported, we will make it a bit smarter...
+#define FET_T_72A            0v
+#define FET_T_72B            1
+#define FET_T_72A_3PHASE     2
+#define FET_T_72B_3PHASE     3
 
-TOPFET=0
-BOTFET=0
+#Harel
+#5:30 PM (6 hours ago)
+#
+#to me, Benny, Zvisha 
+#Hi,
+#The 3 phase change is implemented on the following Rev (included and until farther notice--> need to support increment in rev for example ELA-2003 B03..B09)
+#ELA-2003 B02
+#ELA-2013 B03
+
+FC=`cat /etc/fet`
+OLDTOP=${FC:4:1}
+OLDBOT=${FC:13:1}
 
 
-TOPPNR=`mainvpd -top -q -p`
-BOTPNR=`mainvpd -bottom -q -p`
 
-if [ ${TOPPNR:4} -eq 2003 ] ; then
-	echo TOPFET 1
-	TOPFET=1
-elif [ ${TOPPNR:4} -eq 2013 ] ; then 
-	echo TOPFET 0
-	TOPFET=0
-fi
+calcfet()
+{
+	if [ $? -eq 0 ] ; then
+		if [ ${PNR:4} -eq 2003 ] ; then
+			#echo FET 1/3
+			if [ ${REV:2} -lt 2 ] ; then
+				FET=1
+			else
+				FET=3
+			fi
+		elif [ ${PNR:4} -eq 2013 ] ; then 
+			#echo FET 0/2
+			if [ ${REV:2} -lt 3 ] ; then
+				FET=0
+			else
+				FET=2
+			fi
+		fi
+	fi
+}
 
-if [ ${BOTPNR:4} -eq 2003 ] ; then
-	BOTFET=1
-	echo BOTFET 1
-elif [ ${BOTPNR:4} -eq 2013 ] ; then
-	echo BOTFET 0
-	BOTFET=0
-fi
+
+# first - TOP
+PNR=`mainvpd -top -q -p`
+REV=`mainvpd -top -q -r`
+FET=${OLDTOP}
+calcfet
+TOPFET=${FET}
+
+# then BOTTOM
+PNR=`mainvpd -bottom -q -p`
+REV=`mainvpd -bottom -q -r`
+FET=${OLDBOT}
+calcfet
+BOTFET=${FET}
+
 
 echo "TOP:${TOPFET} BOTTOM:${BOTFET}" > /etc/fet
+
